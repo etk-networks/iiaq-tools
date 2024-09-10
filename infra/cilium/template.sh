5ad41124c8cbf0a8d5238a9254cd8425ecafe0ee
Amadeus Mader <amader@etkn.de>
2024-09-10 10:59:49 +0200

    feat: :sparkles: added cilium

---
+++ infra/cilium/template.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+helm template \
+  cilium \
+  cilium/cilium \
+  --version 1.16.1 \
+  --namespace kube-system \
+  --set ipam.mode=kubernetes \
+  --set kubeProxyReplacement=true \
+  --set securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
+  --set securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
+  --set cgroup.autoMount.enabled=false \
+  --set cgroup.hostRoot=/sys/fs/cgroup \
+  --set k8sServiceHost=localhost \
+  --set k8sServicePort=7445 \
+  --set hubble.relay.enabled=true \
+  --set hubble.ui.enabled=true \
+  --set ingressController.enabled=true \
+  --set ingressController.loadbalancerMode=dedicated \
+  --set hubble.tls.auto.enabled=true \
+  --set hubble.tls.auto.method=certmanager \
+  --set hubble.tls.auto.certValidityDuration=1095 \
+  --set hubble.tls.auto.certManagerIssuerRef.group="cert-manager.io" \
+  --set hubble.tls.auto.certManagerIssuerRef.kind="ClusterIssuer" \
+  --set hubble.tls.auto.certManagerIssuerRef.name="ca-issuer" \
+  --set loadBalancer.l7.backend=envoy >cilium1.16.1.yaml
